33dc08610c18d5175e779383b785b7f594242a3c
Revangga <revanggaprakusya527@gmail.com>
2023-11-17 21:24:08 +0700

    task

--- src/main/kotlin/Exercise4.kt
+++ src/main/kotlin/Exercise4.kt
@@ -1,8 +1,16 @@
 fun main() {
-
+    println(divide(8,6))
 }
 /**
  * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
  *
  */
 // Buat di bawah sini
+fun divide(a: Int, b: Int): Int {
+    return try {
+        a / b
+    } catch (e: ArithmeticException) {
+        0
+    }
+}
+
